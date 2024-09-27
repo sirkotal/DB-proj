04b19aff88a92d4b842096847c53d0817a5e06cf
Miguel Garrido <93833262+sirkotal@users.noreply.github.com>
2024-09-27 12:10:04 +0100

    Added a script to create the database

---
+++ create_db.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+if [[ -e "./test.db" ]]; then
+    echo "Database already exists"
+    exit 1
+fi
+
+sqlite3 ./test.db < ./criar.sql
+sqlite3 ./test.db < ./povoar.sql
+echo "Database Created"
